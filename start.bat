d8e1a66e28264727f9e1547ca6ec7eb6a3a172be
Khiladi2020 <ravindermaan0123@gmail.com>
2020-07-19 23:06:59 +0530

    updated

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+cd D:\Programming Stuff\Udacity\Full stack\Flask App\virt\Scripts
+activate & cd D:\Programming Stuff\Udacity\Solution Projects\Udacity-Full-Stack-Developer-Nanodegree\project01_fyyur\finished & set FLASK_APP=app.py & set FLASK_ENV=development & set FLASK_DEBUG=1 & flask run
---
+++ virt_cmd.bat
@@ -0,0 +1,3 @@
+@echo off
+cd D:\Programming Stuff\Udacity\Full stack\Flask App\virt\Scripts
+activate & cd D:\Programming Stuff\Udacity\Solution Projects\Udacity-Full-Stack-Developer-Nanodegree\project01_fyyur\finished & set PGUSER=postgres& cmd /k & cls
